d3dd5cbfef3770c7b6b8760a6ffc711b26f50b33
Hadar Sarusi <hadarsarusi21@gmail.com>
2024-05-12 13:23:09 +0300

    Stage 2.1 PointTest

--- unittests/primitives/PointTest.java
+++ unittests/primitives/PointTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 /**
- * Testing Point
+ * Unit teats for primitives.Point class
  * @author Lea & Hadar
  */
 
@@ -16,35 +16,44 @@ class PointTest {
     public static final Vector vector = new Vector(1,1,1);
 
 
-
+    /**
+     * Test method for {@link primitives.Point#add(Vector)}
+     */
     @Test
     void add() {
         // ============ Equivalence Partitions Tests ==============
+        //TC01: test if Add vector to point correct
         assertEquals(vector, new Point(2,2,2).add(new Vector(-1,-1,-1)));
         // ============ BVA ==============
+        //TC11: test if center of coordinates does work correctly
         assertEquals(ZERO, ONE.add(new Vector(-1,-1,-1)));
     }
 
+    /**
+     * Test method for {@link primitives.Point#subtract(Point)}
+     */
     @Test
     void subtract() {
         // ============ Equivalence Partitions Tests ==============
-        // TC01: simple test
+        // TC01: test if subtract point to point correct
         assertEquals(ONE, new Point(2,2,2).subtract(ONE));
         // ============ BVA  ==============
-        assertNotEquals(ONE, ONE.subtract(ONE),"\"ERROR: (point - itself) does not throw an exception\"");
-
+        //TC11: test zero vector from subtract
+        assertThrows(IllegalArgumentException.class, ()-> ONE.subtract(ONE),"\"ERROR: (point - itself) does not throw an exception\"");
     }
 
-    private void assertEquals(Point point, Object o) {
-
-    }
-
-    @Test
-    void product() {
-    }
-
+    /**
+     * Test method for {@link primitives.Point#distanceSquared(Point)}
+     */
     @Test
     void distanceSquared() {
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: test if point squared distance to itself is zero
+
+
+
+
+
     }
 
     @Test
