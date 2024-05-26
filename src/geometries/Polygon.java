d2f75cb857318ea44a923d6b1c075e1fc2d45dfa
lea haim <leahaim36@gmail.com>
2024-05-26 20:35:27 +0300

    stage3 sphera BVA test

--- src/geometries/Polygon.java
+++ src/geometries/Polygon.java
@@ -91,6 +91,6 @@ public class Polygon implements Geometry {
     public Vector getNormal(Point point) {
         return plane.getNormal();
     }
-    public List<Point> findIntsersections(Ray ray){return null;}
+    public List<Point> findIntersections(Ray ray){return null;}
 
 }
--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -38,6 +38,35 @@ public class Sphere extends RadialGeometry {
     public Vector getNormal(Point point) {
         return point.subtract(center).normalize();
     }
-    public List<Point> findIntersections(Ray ray){return null;}
+
+    public List<Point> findIntersections(Ray ray) {
+        Point p0 = ray.getHead();
+        Vector v = ray.getDirection();
+        Vector u=this.center.subtract(p0);
+        double tm=v.dotProduct(u);
+        double d=Math.sqrt((u.lengthSquared())-(Math.pow(tm,2)));
+        double th=Math.sqrt((Math.pow(this.radius,2))-(Math.pow(d,2)));
+        if(d >= radius){
+            return null;//no intersection
+        }
+        double t1 = tm - th;
+        double t2 = tm + th;
+        if(t1 < 0){
+            return List.of(p0.add(v.scale(t2)));
+        }
+        if(t2 < 0){
+            return List.of(p0.add(v.scale(t1)));
+        }
+        Point p1 = p0.add(v.scale(t1));
+        Point p2 = p0.add(v.scale(t2));
+        double d1 = p0.distance(p1);
+        double d2 = p0.distance(p2);
+        if(d1 > d2){
+           return List.of(p2,p1);
+        }
+        else{
+            return List.of(p1,p2);
+        }
+    }
 }
 
