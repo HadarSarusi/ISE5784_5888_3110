374ce90ffbfd2cfd4069753dbf3af17e8cf7692e
lea haim <leahaim36@gmail.com>
2024-06-10 20:48:15 +0300

    stage 4 intersection test

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -15,7 +15,8 @@ public class Camera implements Cloneable {
     private double height = 0.0;
     private double distance = 0.0;
 
-    private Camera() {}
+    private Camera() {
+    }
 
     public double getWidth() {
         return width;
@@ -45,8 +46,20 @@ public class Camera implements Cloneable {
         return new Builder();
     }
 
-    public Ray constructRay(int nX, int nY, int j, int i) {
-        return null;
+    public Ray constructRay(int nX, int nY, int i, int j) {
+        Point pC = p0.add(vTo.scale(distance));
+        double Ry = height / nY;
+        double Rx = width / nX;
+        double yi = -(i - (nY - 1) / 2) * Ry;
+        double xj = (j - (nX - 1) / 2) * Rx;
+        // in the beginning pIJ is the center pixel, and if we need to move up and down or right and left
+        Point pIJ = pC;
+        if (!isZero(xj)) pIJ = pIJ.add(vRight.scale(xj));
+        if (!isZero(yi)) pIJ = pIJ.add(vUp.scale(yi));
+
+        Vector vIJ = pIJ.subtract(p0);
+        return new Ray(p0, vIJ);
+
     }
 
     public static class Builder {
@@ -88,19 +101,19 @@ public class Camera implements Cloneable {
             if (Util.alignZero(camera.height) <= 0) throw new IllegalArgumentException("height can't be negative or 0");
             if (Util.alignZero(camera.distance) <= 0)
                 throw new IllegalArgumentException("distance can't be negative or 0");
+            camera.vRight = camera.vTo.crossProduct(camera.vUp).normalize();
             if (!isZero(camera.vRight.dotProduct(camera.vTo)))
                 throw new IllegalArgumentException("wrong values in vRight");
-            camera.vRight = camera.vTo.crossProduct(camera.vUp).normalize();
             //camera.viewPlanePC = camera.p0.add(camera.vTo.scale(camera.distance));
             try {
                 return (Camera) camera.clone();
             } catch (CloneNotSupportedException e) {
                 return null;
             }
+        }
+
+
     }
-
-
-}
 
 
 }
--- unittests/renderer/IntegrationTest.java
+++ unittests/renderer/IntegrationTest.java
@@ -1,17 +1,76 @@
 package renderer;
 
+import geometries.Intersectable;
 import geometries.Sphere;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 import primitives.Point;
+import primitives.Ray;
 import primitives.Vector;
 
 import java.security.KeyStore;
+import java.util.List;
 
 class IntegrationTest {
+
+    private static final int Nx = 3;
+    private static final int Ny = 3;
+
+
+    int helpTest(Camera camera, Intersectable geometry) {
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                Ray ray = camera.constructRay(3, 3, i, j);
+                if (geometry.findIntersections(ray) != null)
+                    count += geometry.findIntersections(ray).size();
+            }
+        }
+        return count;
+    }
+
     @Test
     void sphereIntegrationTest() {
-        //Sphere sphere = new Sphere(new Point(0,0,-3), 1d);//האם הבנאי של ספירה צריך להיות פרטי
-        //Camera camera = new KeyStore.Builder.newInstance();
+
+        //TC01: Small Sphere 2 points
+        Camera camera = new Camera.Builder().
+                setLocation(new Point(0, 0, 0))
+                .setVpDistance(1d)
+                .setDirection(new Vector(0, 1, 0), new Vector(0, 0, -1)).
+                setVpSize(3d, 3d).build();
+        Sphere sphere = new Sphere(new Point(0, 0, -3), 1d);
+        assertEquals(2, helpTest(camera, sphere));
+        //TC02: Big Sphere 18 points
+        Camera camera1 = new Camera.Builder().setLocation(new Point(0, 0, 0.5)).
+                setVpDistance(1d)
+                .setDirection(new Vector(0, 1, 0.5), new Vector(0, 0, -1.5)).
+                setVpSize(3d, 3d).build();
+        Sphere sphere1 = new Sphere(new Point(0, 0, -2.5), 2.5);
+        assertEquals(18, helpTest(camera1, sphere1));
+        //TC03: Medium Sphere 10 points
+        Camera camera2 = new Camera.Builder().setLocation(new Point(0, 0, 0.5)).
+                setVpDistance(1d)
+                .setDirection(new Vector(0, 1, 0), new Vector(0, 0, -1)).
+                setVpSize(3d, 3d).build();
+        Sphere sphere2 = new Sphere(new Point(0, 0, -2), 2);
+        assertEquals(10, helpTest(camera2, sphere2));
+        //TC04: Inside Sphere 9 points
+        Camera camera3 = new Camera.Builder().setLocation(new Point(0, 0, 0.5)).
+                setVpDistance(1d)
+                .setDirection(new Vector(0, 1, 0), new Vector(0, 0, -1)).
+                setVpSize(3d, 3d).build();
+        Sphere sphere3 = new Sphere(new Point(0, 0, -2), 4);
+        assertEquals(10, helpTest(camera3, sphere3));
+        // TC05: Beyond Sphere 0 points
+        Camera camera4 = new Camera.Builder().setLocation(new Point(0, 0, 0.5)).
+                setVpDistance(1d)
+                .setDirection(new Vector(0, 1, 0), new Vector(0, 0, -1)).
+                setVpSize(3d, 3d).build();
+        Sphere sphere4 = new Sphere(new Point(0, 0, 1), 0.5);
+        assertEquals(0, helpTest(camera4, sphere4));
+
 
     }
 
