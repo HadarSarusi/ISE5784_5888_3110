4a890cce86ee01d84cdbb329ff5f56503248919d
lea haim <leahaim36@gmail.com>
2024-07-14 10:10:27 +0300

    stage 7 finalImage

--- unittests/renderer/ReflectionRefractionTests.java
+++ unittests/renderer/ReflectionRefractionTests.java
@@ -51,7 +51,6 @@ public class ReflectionRefractionTests {
         cameraBuilder.setLocation(new Point(0, 0, 1000)).setVpDistance(1000d)
                 .setVpSize(150d, 150d)
                 .setImageWriter(new ImageWriter("refractionTwoSpheres", 500, 500))
-//                .setImageWriter(new ImageWriter("refractionTwoSpheres", 1, 1)) // debug
                 .build()
                 .renderImage()
                 .writeToImage();
@@ -121,51 +120,25 @@ public class ReflectionRefractionTests {
      * This method sets up a scene with ambient light, geometries (triangles and spheres),
      * and multiple light sources, then renders the image using a camera.
      */
-    @Test
-    public void complexImageTest() {
-        // Create a camera object with position, direction, and up vector
-         final Camera.Builder cameraBuilder1 = Camera.getBuilder()
-                .setDirection(new Vector(0, 0, -1), Vector.Y)
-                .setRayTracer(new SimpleRayTracer(scene));
-        // Set the ambient light in the scene
-        scene.setAmbientLight(new AmbientLight(new Color(WHITE), 0.15));
-
-        // Add geometries to the scene
+@Test
+    public void finalScene() {
         scene.geometries.add(
-                // Add a triangle with three points and set its material properties
-                new Triangle(new Point(-150, -150, -115), new Point(150, -150, -135), new Point(75, 75, -150))
-                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60).setKr(0.09)),
-
-                // Add another triangle with three points and set its material properties
+                new Sphere(new Point(0, 0, -50), 50d).setEmission(new Color(BLUE))
+                        .setMaterial(new Material().setKd(0.4).setKs(0.3).setShininess(100).setKt(0.3)),
+                new Sphere(new Point(0, 0, -50), 25d).setEmission(new Color(RED))
+                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(100))
+        ,new Triangle(new Point(-150, -150, -115), new Point(150, -150, -135),
+                new Point(75, 75, -150))
+                .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60)),
                 new Triangle(new Point(-150, -150, -115), new Point(-70, 70, -140), new Point(75, 75, -150))
-                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60).setKr(0.09)),
+                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60)));
+        scene.lights.add(
+                new SpotLight(new Color(1000, 600, 0), new Point(-100, -100, 500), new Vector(-1, -1, -2))
+                        .setKl(0.0004).setKq(0.0000006));
 
-                // Add a sphere with radius and center point, and set its emission and material properties
-                new Sphere(new Point(60, 50, -50),30d )
-                        .setEmission(new Color(RED))
-                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKr(0.5)),
-
-                // Add another sphere with radius and center point, and set its emission and material properties
-                new Sphere(new Point(-40, -30, -80),40d)
-                        .setEmission(new Color(GREEN))
-                        .setMaterial(new Material().setKd(0.4).setKs(0.4).setShininess(50).setKt(0.8)),
-
-                // Add another sphere with radius and center point, and set its emission and material properties
-                new Sphere( new Point(-40, -30, -80),20d)
-                        .setEmission(new Color(YELLOW))
-                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(10).setKr(0.8))
-        );
-
-        // Add lights to the scene
-        scene.lights.add(new SpotLight(new Color(400, 400, 400), new Point(-40, -30, 0),
-                new Vector(0, 0, -1)).setKl(0.0005).setKq(0.0005));
-        scene.lights.add(new SpotLight(new Color(800, 800, 800), new Point(60, 50, 0),
-                new Vector(0, 0, -1)).setKl(0.0005).setKq(0.0005));
-        scene.lights.add(new DirectionalLight(new Color(100, 50, 100), new Vector(-1, -1, -1)));
-
-        cameraBuilder1.setLocation(new Point(0, 0, 1000)).setVpSize(200d, 200d)
-                .setVpDistance(1000d)
-                .setImageWriter(new ImageWriter("complexImageTest", 600, 600))
+        cameraBuilder.setLocation(new Point(0, 0, 1000)).setVpDistance(1000d)
+                .setVpSize(150d, 150d)
+                .setImageWriter(new ImageWriter("finalScene", 500, 500))
                 .build()
                 .renderImage()
                 .writeToImage();
